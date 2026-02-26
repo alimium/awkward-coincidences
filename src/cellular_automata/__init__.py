51f2743fe2a3a4719f56fddb27f991f765353057
Ali Mehrabi <alimehrabi002@gmail.com>
2026-02-26 20:15:03 +0330

    fix: export graph cellular automaton

--- src/cellular_automata/__init__.py
+++ src/cellular_automata/__init__.py
@@ -1,8 +1,10 @@
 from .ca import CellularAutomaton, CellValueType, ComputeMode, SimulationLogMode
+from .gca import GraphCellularAutomaton
 
 __all__ = [
     "CellularAutomaton",
     "CellValueType",
     "ComputeMode",
     "SimulationLogMode",
+    "GraphCellularAutomaton",
 ]
